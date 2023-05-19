225774c681d5c6a9e3792620fa3f05b83f0813e0
cipher-edu <ciphereduuz@gmail.com>
2023-05-19 09:50:21 +0500

    add

--- skladik/forms.py
+++ skladik/forms.py
@@ -10,3 +10,48 @@ class NewUserform(UserCreationForm):
         model = User
         fields  = ("username",)
         field_classes = {'username':UsernameField}
+
+#  Ishchi yaratish uchun forma
+class Worker(forms.ModelForm):
+    class Meta:
+        model = Ishchilar
+        fields = "__all__"
+# To'lov turi formasi
+class Cash(forms.ModelForm):
+    
+    class Meta:
+        model = Cash
+        fields = "__all__"
+#servise turlari
+class Service(forms.ModelForm):
+    class Meta:
+        model = ServiceCategory
+        fields = "__all__"
+
+#prodeuct qushish
+class AddProduct(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+#klient registratsiya
+
+class RegisterClient(forms.ModelForm):
+    class Meta:
+
+        model = UserRegister
+        fields = "__all__"
+
+#service Client
+class ReceptionUser(forms.ModelForm):
+    class Meta:
+        model = UserReseption
+        fields =  "__all__"
+
+# service client
+
+class CerviceUser(forms.ModelForm):
+
+    class Meta:
+        model = UserCervice
+        fields = '__all__'
