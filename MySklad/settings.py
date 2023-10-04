5a96f2bb94b57c2a4e74f68e699c19e2e03ea571
CIPHER-EDU <ciphereduuz@gmail.com>
2023-10-04 10:15:48 +0500

    ad

--- MySklad/settings.py
+++ MySklad/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = 'django-insecure-k&bgac!mdiq4bg_nbcyuj-lmakfdxoia4xq*hiy#8p1_^5khj0'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
